3fcf82873781df2088a66eec002a4057660d8eda
Muhammad Tahir <150343129+Tahir-Dars@users.noreply.github.com>
2026-04-17 18:38:34 +0500

    Initial Commit

---
+++ src/main/java/contactmgt/getting/GettingApplication.java
@@ -0,0 +1,13 @@
+package contactmgt.getting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GettingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GettingApplication.class, args);
+    }
+
+}
