a048acdfbadb7ae248577a324c33d66c3e6db571
Muhammad Tahir <150343129+Tahir-Dars@users.noreply.github.com>
2026-04-18 04:04:16 +0500

    Added all the unsecure open to public endpoints

---
+++ src/main/java/controller/CustomerController.java
@@ -0,0 +1,28 @@
+package controller;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/contacts")
+public class CustomerController {
+
+    @GetMapping
+    public String getContacts() {
+        return "Returning All contacts";
+    }
+
+    @PostMapping
+    public String addContacts() {
+        return "New Contact added";
+    }
+
+    @DeleteMapping("/{id}")
+    public String deleteContact(@PathVariable int id) {
+        return "Contact " + id + "deleted";
+    }
+
+    @GetMapping("/public/info")
+    public String publicInfo() {
+        return "This is public endpoint";
+    }
+}
