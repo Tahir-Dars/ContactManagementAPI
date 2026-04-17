c01a9504d033ddc28bd7790a1dc99fd9aba81713
Muhammad Tahir <150343129+Tahir-Dars@users.noreply.github.com>
2026-04-18 04:44:34 +0500

    Added the SecurityConfigurations

---
+++ src/main/java/contactmgt/getting/secConfig/SecurityConfig.java
@@ -0,0 +1,9 @@
+package contactmgt.getting.secConfig;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SecurityConfig {
+
+}
