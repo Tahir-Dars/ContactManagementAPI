e33457be21e86b42aef7b339e7f5b540e87de2ac
Muhammad Tahir <150343129+Tahir-Dars@users.noreply.github.com>
2026-04-18 21:25:02 +0500

    feat: Implemented the method PasswordEncoder

--- src/main/java/contactmgt/getting/secConfig/SecurityConfig.java
+++ src/main/java/contactmgt/getting/secConfig/SecurityConfig.java
@@ -1,15 +1,28 @@
 package contactmgt.getting.secConfig;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    @Autowired
+    DataSource dataSource;
 
     @Bean
     SecurityFilterChain defualtSecurityFilterChain(HttpSecurity httpSecurity) {
@@ -30,5 +43,10 @@ public class SecurityConfig {
         httpSecurity.csrf(csrf -> csrf.disable());
 
         return httpSecurity.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 }
