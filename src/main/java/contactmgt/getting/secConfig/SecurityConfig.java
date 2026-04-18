a23a6a0aabf053f499332b41c883f3164a583bca
Muhammad Tahir <150343129+Tahir-Dars@users.noreply.github.com>
2026-04-18 21:03:58 +0500

    feat: implement default security filter chain in SecurityConfig

--- src/main/java/contactmgt/getting/secConfig/SecurityConfig.java
+++ src/main/java/contactmgt/getting/secConfig/SecurityConfig.java
@@ -2,8 +2,32 @@ package contactmgt.getting.secConfig;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
+    @Bean
+    SecurityFilterChain defualtSecurityFilterChain(HttpSecurity httpSecurity) {
+        httpSecurity.authorizeHttpRequests(authorizeRequests ->
+                authorizeRequests.requestMatchers("/h2-console/**").permitAll()
+                        .anyRequest().authenticated());
+
+        httpSecurity.sessionManagement(
+                session ->
+                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+        );
+
+        httpSecurity.headers(headers -> headers.frameOptions(
+                frameOptions -> frameOptions.sameOrigin()
+        ));
+
+        httpSecurity.csrf(csrf -> csrf.disable());
+
+        return httpSecurity.build();
+    }
 }
